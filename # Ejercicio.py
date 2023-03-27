75e4e282978a675d5950096aa484c47ed1da14b8
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-03-27 00:04:34 -0500

    #

--- # Ejercicio.py
+++ # Ejercicio.py
@@ -1,7 +1,11 @@
-# Ejercicio 
+# Ejercicio
 def odd_or_even(n):
-    n = int(input('ingresa el numero entero que quieres'  ver si es odd o even: '))
     if n % 2 == 0:
         print(f'El numero {n} es even')
     else:
         print(f'El numero {n} es even')
+
+
+n = int(input("ingresa el numero entero que quieres "
+        "ver si es odd o even: "))
+odd_or_even(n)
