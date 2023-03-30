4994bba496549512c7a398b0f724dfe437fcc6bb
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-03-30 09:26:05 -0500

    ejercicio de afirmaciones

--- Pruebas_caja_cristal.py
+++ Pruebas_caja_cristal.py
@@ -3,7 +3,8 @@ import unittest
 
 def calcular_descuento(cantidad_artículos):
     """
-    Calcula el descuento a aplicar en función de la cantidad de artículos comprados.
+    Calcula el descuento a aplicar en función de la cantidad de \n
+    artículos comprados.
     """
     if cantidad_artículos >= 10:
         descuento = 0.2
@@ -21,21 +22,24 @@ class PruebaCajaCristal(unittest.TestCase):
 
     def test_descuento_cero(self):
         """
-        Prueba que se devuelve un descuento del 0% para una cantidad de artículos menor que 5.
+        Prueba que se devuelve un descuento del 0% para una cantidad de\n
+        artículos menor que 5.
         """
         descuento = calcular_descuento(3)
         self.assertEqual(descuento, 0.0)
 
     def test_descuento_diez(self):
         """
-        Prueba que se devuelve un descuento del 10% para una cantidad de artículos entre 5 y 9.
+        Prueba que se devuelve un descuento del 10% para una cantidad de \n
+        artículos entre 5 y 9.
         """
         descuento = calcular_descuento(7)
         self.assertEqual(descuento, 0.1)
 
     def test_descuento_veinte(self):
         """
-        Prueba que se devuelve un descuento del 20% para una cantidad de artículos igual o mayor que 10.
+        Prueba que se devuelve un descuento del 20% para una cantidad de \n
+        artículos igual o mayor que 10.
         """
         descuento = calcular_descuento(12)
         self.assertEqual(descuento, 0.2)
