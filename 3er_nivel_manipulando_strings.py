23d87edeba9a9263138eef4aca411437f9706992
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-04-04 14:27:01 -0500

    Reto 1 ok

---
+++ 3er_nivel_manipulando_strings.py
@@ -0,0 +1,4 @@
+# Reto 1 "longitud del string"
+curso = str(input('Por favor ingresa tu curso favorito: '))
+longitud = len(curso)
+print(f'El curso que ingresaste es  un string de {longitud} caracteres')
