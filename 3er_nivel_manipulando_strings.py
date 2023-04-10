58c20a48493c47cf11416010a722f9894fd372da
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-04-09 21:23:05 -0500

    Reto 7 ok

--- 3er_nivel_manipulando_strings.py
+++ 3er_nivel_manipulando_strings.py
@@ -120,3 +120,16 @@ else:
     apellido_capitalizado = ' '.join(capitalizadas)
     print(f'Hola como estas {nombre_capitalizado} {apellido_capitalizado}')
 # Reto 7 "Hablemos Pig Latin! (Puerco Latino)"
+palabra = input('Ingresa una palabra que queras traducir a PigLatin: ')
+vocales = ["a", "e", "i", "o", "u"]
+if palabra[0].lower() in vocales:
+    palabra_resultante = palabra + 'way'
+else:
+    primera_consonante = ''
+    for letra in palabra:
+        if letra.lower() not in vocales:
+            primera_consonante += letra
+        else:
+            break
+    palabra_resultante = palabra[len(primera_consonante):] + primera_consonante + 'ay'
+print(f'La palabra en PigLatin es: {palabra_resultante}')
