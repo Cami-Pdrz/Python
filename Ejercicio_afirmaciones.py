93aa03a0908e5c30730cc93e6d8e00ff7974a8b0
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-04-11 10:45:09 -0500

    Primer avance

--- Ejercicio_afirmaciones.py
+++ Ejercicio_afirmaciones.py
@@ -1,10 +1,3 @@
-# assert <expresion  booleana>, <mensaje de error>
-def primera_letra(lista_de_palabras):
-    primeras_letras = []
-
-    for palabra in lista_de_palabras:
-        assert type(palabra) == str, f'{palabra} no es un str'
-        assert len(palabra) > 0, 'no se permite str vacios'
-
-        primeras_letras.append(palabra[0])
-    return primeras_letras
+a = True
+b = False
+print (a and b) 
