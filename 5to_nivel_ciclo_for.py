c85acbf5991159f5aa3fd5854a06bdc7a8ee67f4
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-04-14 19:12:04 -0500

    Reto 2 ok

--- 5to_nivel_ciclo_for.py
+++ 5to_nivel_ciclo_for.py
@@ -2,3 +2,12 @@
 curso = input('Por favor ingresa tu curso de Platzi favorito: ')
 for i in range(8):
     print(curso)
+# Reto 2 "Curso favorito ‘n’ veces"
+curso = input('Por favor ingresa tu curso de Platzi favorito: ')
+veces = int(input('Por favor ingres el numero de veces que quieres mostrar el'
+                  f' curso: '))
+while veces <= 0:
+    veces = int(input('Se admiten números mayores a 0, intenta de nuevo: '))
+for i in range(veces):
+    print(curso)
+# Reto 
