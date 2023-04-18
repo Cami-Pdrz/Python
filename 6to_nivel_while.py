8ea1c36e86bc7f6a5fe2a96d8da7278da983503d
Camilo Pedroza <cpedrozamesa@gmail.com>
2023-04-17 21:38:03 -0500

    Reto 2 ok

--- 6to_nivel_while.py
+++ 6to_nivel_while.py
@@ -1,4 +1,4 @@
-# Reto1 "Suma hasta 50"
+# Reto 1 "Suma hasta 50"
 total = 0
 while total <= 50:
     num = input('por favor ingresa un numero para sumar al total: ')
@@ -6,4 +6,12 @@ while total <= 50:
         total += int(num)
     else:
         print('error solo se admiten números')
+print('la suma total es:', total)
+# Reto 2 "Más allá del cuarenta y dos"
+num = 0
+total = 0
+while num <= 42:
+    num = int(input('por favor ingresa otro numero: '))
+    if num <= 42:
+        total += num
 print('la suma total es:', total)
